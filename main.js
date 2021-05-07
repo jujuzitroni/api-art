9c1a9f3c240283c64b5cee4ac9c725088bbda25e
jujuzitroni <jujuzitroni@gmail.com>
2021-05-07 12:04:43 +0200

    ✨ Add HTML elements

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 import './style.css';
+import { createElement } from './utils/elements';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+const headerElement = createElement('header', {
+  className: 'hero',
+  children: [
+    createElement('h1', { className: 'hero-h1', innerText: 'Look at Art' }),
+    createElement('p', {
+      className: 'hero-text',
+      innerText:
+        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    }),
+    createElement('button', {
+      className: 'hero-button',
+      type: 'button',
+      innerText: 'get yourself some art',
+    }),
+  ],
+});
+
+const resultsElement = createElement('section', {
+  className: 'resultsSection',
+});
+
+const footerElement = createElement('footer', {
+  className: 'footer',
+  children: [
+    createElement('p', {
+      className: 'footer-text',
+      innerText: "It's fine. This is fine.",
+    }),
+  ],
+});
+
+const mainElement = createElement('main', {
+  className: 'main',
+  children: [headerElement, resultsElement, footerElement],
+});
+
+document.querySelector('#app').append(mainElement);
